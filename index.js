799227cb52b281a33de5f620c00693d069256994
Miller Rich <william.miller.rich@gmail.com>
2020-09-09 21:18:28 -0500

    add question array, create function init and add inquirer.prompt(questions)

--- index.js
+++ index.js
@@ -1,2 +1,70 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+
+// questions to generate README
+const questions = [
+    {
+        type: "input",
+        message: "What is your project title?",
+        name: "Title"
+    },
+    {
+        type: "input",
+        message: "Describe your project",
+        name: "Description"
+    },
+    {
+        type: "input",
+        message: "What are the steps required to install your project?",
+        name: "Installation"
+    },
+    {
+        type: "input",
+        message: "Provide instructions for use.",
+        name: "Usage"
+    },
+    {
+        type: "list",
+        message: "Select license",
+        name: "License",
+        choices: [
+            "MIT",
+            "GVL-GPL 3.0",
+            "APACHE 2.0",
+            "BSD 3",
+            "None"
+        ]
+    },
+    {
+        type: "input",
+        message: "Contributors?",
+        name: "Contributors"
+    },
+    {
+        type: "input",
+        message: "How do you test your project?",
+        name: "Test"
+    },
+    
+    {
+        type: "input",
+        message: "Contact info for further questions",
+        name: "Questions"
+    },
+    {
+        type: "input",
+        message: "Your Github username",
+        name: "Username"
+    },
+    {
+        type: "input",
+        message: "What is your email?",
+        name: "Email"
+    }
+    
+];
+
+function init() {
+    inquirer.prompt(questions);
+}
+init();
